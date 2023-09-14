20a36c62dda363e646f81d5afd3461f15aafb71d
haotian-chen-ooh <haotian.chen@oohmedia.com.au>
2023-09-14 14:03:29 +1000

    sherpa

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import mainLogo from "../../imgs/logo.png";
+import sherpaLogo from "../../imgs/sherpa.png";
 import "./Header.css";
 
 export const Header = () => {
   return (
     <div className="header-container">
       <h1 className="title">System Sherpa</h1>
-      <img className="logo" src={mainLogo} alt="logo" />
+      <div>
+        <img className="logo" src={mainLogo} alt="logo" />
+        <img className="sherpa-logo" src={sherpaLogo} alt="sherpa" />
+      </div>
     </div>
   );
 };
